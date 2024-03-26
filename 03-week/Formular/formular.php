a6b01ac5d122ce24317346fb41cddc37b8248938
NasaboyZ <josefleite.00@hotmail.com>
2024-03-26 11:15:11 +0100

    implementierung der countrie Wischlist

--- 03-week/Formular/formular.php
+++ 03-week/Formular/formular.php
@@ -1,30 +1,29 @@
 <?php 
 $pageTitle = basename($_SERVER['PHP_SELF'], '.php');
 ?>
-
-
+    <?php include 'länder/country.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php $pageTitle ?></title>
+    <title><?php echo $pageTitle ?></title>
 
     <style>
-        body *{
-            ox-sizing: border-box;
+        body * {
+            box-sizing: border-box;
             margin: 0;
             padding: 0;
         }
-        body{
-            background: rgba(223 230 231);
+        body {
+            background: rgba(223, 230, 231);
             width: 100%;
             font-family: Arial, Helvetica, sans-serif;
             height: 100vh;
         }
         
-        nav{
+        nav {
             display: flex;
             align-items: center;
             justify-content: space-between;
@@ -32,13 +31,13 @@ $pageTitle = basename($_SERVER['PHP_SELF'], '.php');
             padding: 0 25px;
             height: 10vh;
         }
-        .container-for-interaction{
+        .container-for-interaction {
             display: flex;
             align-items: center;
-             gap: 2rem;
+            gap: 2rem;
         }
 
-        .container-for-interaction > a{
+        .container-for-interaction > a {
             text-decoration: none;
             color: black;
             padding: 7px 27px 7px 27px;
@@ -46,8 +45,7 @@ $pageTitle = basename($_SERVER['PHP_SELF'], '.php');
             background: yellow;
         }
 
-
-        .form-kontakt{
+        .form-kontakt {
             gap: 2rem;
             display: flex;
             align-items: center;
@@ -56,12 +54,11 @@ $pageTitle = basename($_SERVER['PHP_SELF'], '.php');
             width: 40%;
         }
 
-        a{
+        a {
             text-decoration: none;
-             color: black;
-
+            color: black;
         }
-        .continer-anrede{
+        .continer-anrede {
             display:flex;
             gap: 1rem;
             margin-left:2rem;
@@ -71,92 +68,92 @@ $pageTitle = basename($_SERVER['PHP_SELF'], '.php');
 <body>
     <header>
         <nav>
-        <div class="logo-in-login"><a href="index.html">M.W.C</a></div>
-        <div class="container-for-interaction">
-            <p class="frage-an-user">Do you already have an account?</p>
-            <a href="login.html" class="btn-login">Login</a>
-        </div>
+            <div class="logo-in-login"><a href="index.html">M.W.C</a></div>
+            <div class="container-for-interaction">
+                <p class="frage-an-user">Do you already have an account?</p>
+                <a href="login.html" class="btn-login">Login</a>
+            </div>
         </nav>
     </header>
     
-
     <main> 
-    <div class="container-form">
+        <div class="container-form">
+            <form class="form-kontakt" action="validation.php" method="POST">
+                <div class="continer-anrede">
+                    <input type="radio" id="male" name="anrede" value="Herr">
+                    <label for="male">Herr</label> <br>
 
-        <form class="form-kontakt" action="validation.php" method="POST">
-        <div class="continer-anrede">
-            <input type="radio" id="html" name="fav_language" value="anredeer">
-             <label for="news-letter">Er</label> <br>
+                    <input type="radio" id="female" name="anrede" value="Frau">
+                    <label for="female">Frau</label> <br>
+                    
+                    <input type="radio" id="other" name="anrede" value="Andere">
+                    <label for="other">Andere</label>
+                </div>
 
-<input type="radio" id="html" name="fav_language" value="anredesie">
-             <label for="news-letter">Sie</label> <br>
-<input type="radio" id="html" name="fav_language" value="nonbinar">
-             <label for="news-letter">nonbinare</label>
-    </div>
-            <div class="container-firstname">
-                <label for="firstname"></label>
-                <input class="focusout" type="text" name="firstname" id="firstname" placeholder="Vorname" >
-            </div>
-            <div class="container-secondname">
-                <label for="secondName"></label>
-                <input class="focusout" type="text" name="secondName" id="secondName" placeholder="Nachname" >
-            </div>
-            <select class="länder" name="#" id="">
-                <option value="# my-4">länder</option>
-                <option value="#">Schweiz</option>
-                <option value="#">Camarun</option>
-                <option value="#">Frankreich</option>
-                <option value="#">Weissrussland</option>
-            </select>
-            <div class="container-adresse">
-                <label for="adresse"></label>
-                <input class="focusout" type="text" name="adresse" id="adresse" placeholder="Adresse" >
-            </div>
-            <div class="container-plz">
-                <label for="plz"></label>
-                <input class="focusout" type="text" name="plz" id="plz" placeholder="Postleitzahl" >
-            </div>
-            <div class="container-ort">
-                <label for="ort"></label>
-                <input class="focusout" type="text" name="ort" id="ort" placeholder="Ort" >
-            </div>
-            <div class="continer-email">
-                <label for="email"></label>
-                <input class="focusout" type="email" name="email" id="email" placeholder="Email" >
-            </div>
-            <div class="continer-password">
-                <label for="password"></label>
-                <input class="focusout" type="password" name="password" id="password" placeholder="Password" >
-            </div>
-            <div class="continer-agb">
-            <label class="container">AGB
-            <input type="checkbox" checked="checked">
-            <span class="checkmark"></span>
-            </label>
-            </div>
+                <div class="container-firstname">
+                    <input class="focusout" type="text" name="firstname" id="firstname" placeholder="Vorname" >
+                </div>
 
-            <div class="continer-newsletter">
-            <input type="radio" id="html" name="fav_language" value="news-letter">
-             <label for="news-letter">Ich möchte den Newsletter abonieren.</label>
-            </div>
-            <div class="container-messsage">
-                <label for="message"></label>
-                <textarea name="message" id="message" placeholder="Nachricht"></textarea>
-            </div>
-            <div class="container-submit">
-                <input class="focusout" id="submit" type="submit" value="Subscribe!">
-            </div>
-        </form>
-    </div>
+                <div class="container-secondname">
+                    <input class="focusout" type="text" name="secondName" id="secondName" placeholder="Nachname" >
+                </div>
+
+                <div class="container-länder">
+                    <label for="city">Bitte wählen Sie die Stadt aus:</label>
+                    <select name="city" id="city">
+                        <?php foreach($countries as $key => $value): ?>
+                            <option value="<?php echo $key; ?>" <?php if(isset($_POST['city']) && $_POST['city']===$key) echo 'selected'; ?>><?php echo $value; ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+
+                <div class="container-adresse">
+                    <input class="focusout" type="text" name="adresse" id="adresse" placeholder="Adresse" >
+                </div>
+
+                <div class="container-plz">
+                    <input class="focusout" type="text" name="plz" id="plz" placeholder="Postleitzahl" >
+                </div>
+
+                <div class="container-ort">
+                    <input class="focusout" type="text" name="ort" id="ort" placeholder="Ort" >
+                </div>
+
+                <div class="container-email">
+                    <input class="focusout" type="email" name="email" id="email" placeholder="Email" >
+                </div>
+
+                <div class="container-password">
+                    <input class="focusout" type="password" name="password" id="password" placeholder="Password" >
+                </div>
+
+                <div class="container-agb">
+                    <label class="container">AGB
+                        <input type="checkbox" checked="checked">
+                        <span class="checkmark"></span>
+                    </label>
+                </div>
+
+                <div class="container-newsletter">
+                    <input type="checkbox" id="newsletter" name="newsletter" value="newsletter">
+                    <label for="newsletter">Ich möchte den Newsletter abonieren.</label>
+                </div>
+
+                <div class="container-messsage">
+                    <textarea name="message" id="message" placeholder="Nachricht"></textarea>
+                </div>
+
+                <div class="container-submit">
+                    <input class="focusout" id="submit" type="submit" value="Abschicken!">
+                </div>
+            </form>
+        </div>
     </main>
+
+    <?php 
+    if(!empty($_POST) && isset($cities[$_POST['city']])) {
+        include "text/{$_POST['city']}.html";
+    }
+    ?>
 </body>
 </html>
-<?php
-
-// auslesen von Daten aus einem GET Formular;
-var_dump($_GET);
-
-
-//auslesen von Daten mit eine post formular;
-var_dump($_POST);
-?>
--- 03-week/Formular/länder/country.php
+++ 03-week/Formular/länder/country.php
@@ -1,4 +1,32 @@
 <?php
-$länder =[
-    'Swiss',
-]
+    $countries =[
+        'hungary' => 'Ungarn',
+        'finland' => 'Finnland',
+        'switzerland' => 'Schweiz',
+        'united_kingdom' => 'Vereinigtes Königreich',
+        'france' => 'Frankreich',
+        'italy' => 'Italien',
+        'germany' => 'Deutschland',
+        'spain' => 'Spanien',
+        'portugal' => 'Portugal',
+        'netherlands' => 'Niederlande',
+        'austria' => 'Österreich',
+        'czech_republic' => 'Tschechien',
+        'belgium' => 'Belgien',
+        'russia' => 'Russland',
+        'greece' => 'Griechenland',
+        'sweden' => 'Schweden',
+        'norway' => 'Norwegen',
+        'denmark' => 'Dänemark',
+        'ireland' => 'Irland',
+        'poland' => 'Polen',
+        'romania' => 'Rumänien',
+        'croatia' => 'Kroatien',
+        'slovakia' => 'Slowakei',
+        'slovenia' => 'Slowenien',
+        'serbia' => 'Serbien',
+        'bulgaria' => 'Bulgarien',
+        'turkey' => 'Türkei'
+    ];
+    ksort($countries); // ksort ist ein tag der mir erlaubt alles alpha betisch aus zugeben in meiner liste
+?>
